0ed6e9e2134d9030ebbd5efd73fb171f250787d4
Abigail Borromeo <129239019+abbeeeeyy@users.noreply.github.com>
2023-05-11 20:53:43 +0800

    Create App.java

---
+++ Exercises/Exercise3/Dog/App.java
@@ -0,0 +1,13 @@
+package Dog;
+
+public class App {
+    
+    public static void main(String[] args) throws Exception {
+
+        Pomeranian myPet = new Pomeranian();
+ 
+         myPet.bark();
+         System.out.println(myPet.mouth);
+         System.out.println(myPet.bark);
+     }
+ }
